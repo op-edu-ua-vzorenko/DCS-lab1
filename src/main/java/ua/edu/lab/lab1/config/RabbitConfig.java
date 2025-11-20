ba88ea608d24dfe37936b1bdd700e2f6214627ec
Vasyl Zorenko <vasiliy.zorenko@stud.op.edu.ua>
2025-11-20 03:45:54 +0200

    Add rabbitMQ config and dependencies

---
+++ src/main/java/ua/edu/lab/lab1/config/RabbitConfig.java
@@ -0,0 +1,40 @@
+package ua.edu.lab.lab1.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String QUEUE = "lab1_enrich_queue";
+    public static final String EXCHANGE = "lab1_exchange";
+    public static final String ROUTING_KEY = "post.created";
+
+    @Bean
+    public Queue queue() { return new Queue(QUEUE, true); }
+
+    @Bean
+    public TopicExchange exchange() { return new TopicExchange(EXCHANGE); }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    // Самое важное: учим Java работать с JSON
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory cf) {
+        RabbitTemplate template = new RabbitTemplate(cf);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
